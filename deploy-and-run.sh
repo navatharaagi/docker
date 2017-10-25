09fb05f092c9f08295789d54aa1a4a4e9f2a3da7
Navatha Raagi <navatharaagi@gmail.com>
2017-10-25 11:59:38 -0700

    Updated deploy-and-run.sh.

--- deploy-and-run.sh
+++ deploy-and-run.sh
@@ -19,4 +19,4 @@ fi
 
 # Use faster (though more unsecure) random number generator
 #export CATALINA_OPTS="${CATALINA_OPTS} $(agent-bond-opts --escape) -Djava.security.egd=file:/dev/./urandom"
-/opt/tomcat/bin/catalina.sh run
+#/opt/tomcat/bin/catalina.sh run
