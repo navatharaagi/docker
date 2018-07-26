6f2818fb46de1641d98b2fcf662eebba95cad4f5
Navatha Raagi <navatharaagi@gmail.com>
2018-07-26 11:22:48 -0700

    Updated tomcatinitscript.sh.

--- tomcatinitscript.sh
+++ tomcatinitscript.sh
@@ -4,10 +4,10 @@
 #description: Starts/Stops Apache Tomcat
 #
 #Tomcat 7 start/stop/status script
-# Forked from: https://gist.github.com/valotas/1000094
-# @author: Miglen Evlogiev <bash@miglen.com>
+#Forked from: https://gist.github.com/valotas/1000094
+#@author: Miglen Evlogiev <bash@miglen.com>
 #
-# Release updates:
+#Release updates:
 # Updated method for gathering pid of the current proccess
 # Added usage of CATALINA_BASE
 # Added coloring and additional status
