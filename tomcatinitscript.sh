6ba9b18c7aee70d945ec218dbccd984d435c5390
Navatha Raagi <navatharaagi@gmail.com>
2018-07-30 10:50:06 -0700

    Updated tomcatinitscript.sh.

--- tomcatinitscript.sh
+++ tomcatinitscript.sh
@@ -28,13 +28,13 @@ export CATALINA_HOME=/usr/share/tomcat
 # CATALINA_BASE is the location of the configuration files of this instance of Tomcat
 export CATALINA_BASE=/usr/share/tomcat
 
-#TOMCAT_USER is the default user of tomcat
+# TOMCAT_USER is the default user of tomcat
 export TOMCAT_USER=tomcat
 
-#TOMCAT_USAGE is the message if this script is called without any options
+# TOMCAT_USAGE is the message if this script is called without any options
 TOMCAT_USAGE="Usage: $0 {\e[00;32mstart\e[00m|\e[00;31mstop\e[00m|\e[00;32mstatus\e[00m|\e[00;31mrestart\e[00m}"
 
-#SHUTDOWN_WAIT is wait time in seconds for java proccess to stop
+# SHUTDOWN_WAIT is wait time in seconds for java proccess to stop
 SHUTDOWN_WAIT=20
 
 tomcat_pid() {
